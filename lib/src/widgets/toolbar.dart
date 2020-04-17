183f6cbd7f29d84c206f2ed7456192e5713c47a5
Prasanthi99 <31959931+Prasanthi99@users.noreply.github.com>
2020-04-17 19:30:17 +0530

    Update toolbar.dart
    Removed heading button

--- lib/src/widgets/toolbar.dart
+++ lib/src/widgets/toolbar.dart
@@ -243,8 +243,7 @@ class ZefyrToolbarState extends State<ZefyrToolbar>
     final buttons = <Widget>[
       buildButton(context, ZefyrToolbarAction.bold),
       buildButton(context, ZefyrToolbarAction.italic),
-      LinkButton(),
-      HeadingButton(),
+      LinkButton(),     
       if (editor.imageDelegate != null) ImageButton(),
     ];
     return buttons;
